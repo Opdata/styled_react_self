ef3fe8c2e7d68dcc81557af4f7ed46357fa78bd5
Opdata <pb03641@gmail.com>
2020-02-22 21:54:57 +0900

    [test] CSSModule 사용과 classnames 사용으로 스타일 적용 테스트

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,10 +1,13 @@
 import React from "react";
+import classNames from "classnames/bind";
 import styles from "./CSSModule.module.css";
+
+const cx = classNames.bind(styles); // 미리 styles에서 클래스를 받아 오도록 설정
 
 const CSSModule = () => {
   return (
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
-      안녕하세요. 저는 <span className="something">CSS Module</span>
+    <div className={cs("wrapper", "inverted")}>
+      안녕하세요, 저는 <span className="something">CSS Module</span>
     </div>
   );
 };
